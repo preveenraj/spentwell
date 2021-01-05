c413222419486180696650af395005ba18c687f4
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-01-05 20:30:52 +0530

    data binding sample and recycler view

--- app/src/main/java/com/spentwell/MainActivity.kt
+++ app/src/main/java/com/spentwell/MainActivity.kt
@@ -2,10 +2,31 @@ package com.spentwell
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.spentwell.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val list = mutableListOf<String>()
+        list.add("Sample")
+        list.add("Prabha")
+        list.add("Preveen")
+        list.add("Sample")
+        list.add("Prabha")
+        list.add("Preveen")
+        list.add("Sample")
+        list.add("Prabha")
+        list.add("Preveen")
+        list.add("Sample")
+        list.add("Prabha")
+        list.add("Preveen")
+        binding.recyclerView.adapter = SampleAdapter(list)
     }
 }
